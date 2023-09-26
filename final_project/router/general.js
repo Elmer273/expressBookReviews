84e53515f77571db0c2cd9dadf0dc2c094f42fad
Elmer273 <elmermartinez666@yahoo.com>
2023-09-26 01:31:25 -0400

    Add async-await functions

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const axios = require('axios');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+const URL = "https://<username>-5000.theiadocker-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai";
 
 public_users.post("/register", (req,res) => {
   const {username, password} = req.body;
@@ -23,11 +25,33 @@ public_users.get('/',function (req, res) {
   return res.status(200).json(books);
 });
 
+// (Async-await) Get the book list available in the shop
+async function getBookList(){
+    try {
+        const response = await axios.get(URL + '/');
+        console.log(response.data);
+        return(response.data);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   const {isbn} = req.params;
   return res.status(200).json(books[isbn]);
 });
+
+// (Async-await) Get book details based on ISBN
+async function getBookByIsbn(isbn){
+    try {
+        const response = await axios.get(URL +'/isbn/' +isbn);
+        console.log(response.data);
+        return(response.data);
+    } catch (error) {
+        console.error(error);
+    }
+}
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
@@ -38,6 +62,17 @@ public_users.get('/author/:author',function (req, res) {
   return res.status(200).json(filteredBooks);
 });
 
+// (Async-await) Get book details based on author
+async function getBookByAuthor(author){
+    try {
+        const response = await axios.get(URL +'/author/' +author);
+        console.log(response.data);
+        return(response.data);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   const { title } = req.params;
@@ -47,6 +82,17 @@ public_users.get('/title/:title',function (req, res) {
   return res.status(200).json(filteredBooks);
 });
 
+// (Async-await) Get book details based on title
+async function getBookByTitle(title){
+    try {
+        const response = await axios.get(URL +'/title/' +title);
+        console.log(response.data);
+        return(response.data);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   const {isbn} = req.params;
@@ -54,3 +100,7 @@ public_users.get('/review/:isbn',function (req, res) {
 });
 
 module.exports.general = public_users;
+module.exports.getBookList = getBookList;
+module.exports.getBookByIsbn = getBookByIsbn;
+module.exports.getBookByAuthor = getBookByAuthor;
+module.exports.getBookByTitle = getBookByTitle;
